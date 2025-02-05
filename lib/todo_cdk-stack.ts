eb5e238f695533cd059e445e35964b4debc0199e
David Amankwa <davidamankwa@gmail.com>
2025-02-05 15:23:12 -0500

    bucket example
    bucket example

--- lib/todo_cdk-stack.ts
+++ lib/todo_cdk-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import {Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number ){
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(expiration)
+      }]
+    });
+  }
+}
 
 export class TodoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //create an L2 s3 Bucket 
+    //this denotes this L2 bucket belongs to "this" () stack 
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(2)
+      }]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration:{
+        rules:[{
+          expirationInDays:1,
+          status: 'Enabled'
+        }]
+      }
+    })
+
+    new L3Bucket(this, 'MyL3Bucket', 3)
+
   }
 }
