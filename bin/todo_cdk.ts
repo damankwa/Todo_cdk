fa7a28c031a50dcf260d531f99188533f3a9d0eb
David Amankwa <davidamankwa@gmail.com>
2025-02-06 15:43:37 -0500

    intrinsic function example
    adding another Stack. Control the name of the Stack

--- bin/todo_cdk.ts
+++ bin/todo_cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { TodoCdkStack } from '../lib/todo_cdk-stack';
+import { PhotosStack } from '../lib/PhotosStack';
 
 const app = new cdk.App();
 new TodoCdkStack(app, 'TodoCdkStack');
+new PhotosStack(app, 'PhotosStack');
---
+++ lib/PhotosStack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import {Bucket} from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import {Fn} from 'aws-cdk-lib';
+
+
+
+export class PhotosStack extends cdk.Stack {
+
+ private stackSuffix: string;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.initializeSuffix();
+
+    const myBucket = new Bucket(this,'PhotosBucket', {
+        bucketName: `photos-bucket-${this.stackSuffix}`
+    });
+  }
+
+  private initializeSuffix(){
+    const shortStackId = Fn.select(2,Fn.split('/', this.stackId));
+    this.stackSuffix =  Fn.select(4,Fn.split('-', shortStackId));
+
+  }
+}
